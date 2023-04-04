3dd393e9b3a66dc477abda22e3418226ac62a782
felixfavour <felixfavour0@gmail.com>
2023-04-04 08:03:44 +0100

    init: pivoted to FE app

---
+++ components/CookiesHome.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="cookies-home">
+    <div class="inner">
+      <h1>Cookies Converter Demo</h1>
+      <h3>Submit to store cookie</h3>
+      <form @submit.prevent>
+        <label for="word">
+          Phrase/Word
+          <input v-model="word" type="text" id="word">
+        </label>
+        <button :disabled="!(email.length > 4 && password.length > 1)" type="submit" @click="submit">
+          Submit
+        </button>
+      </form>
+    </div>
+  </div>
+</template>
+
+<script setup>
+  import Cookies from 'js-cookie'
+  import CryptoJS from 'crypto-js'
+
+  const word = ref('')
+  const password = ref('')
+  const config = useRuntimeConfig()
+
+  const submit = () => {
+    alert('Cookie Stored')
+  }
+</script>
+
+<style scoped>
+  
+  label {
+    display: flex;
+    flex-direction: column;
+    margin-bottom: 2rem;
+  }
+  .cookies-home {
+    display: grid;
+    place-items: center;
+  }
+  .cookies-home .inner {
+    max-width: 400px;
+  }
+</style>
